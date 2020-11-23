ab279122fb5d9859fd37a561fa58786734f92138
sashka732 <sashka732@gmail.com>
2020-11-23 00:20:37 -0500

    Beginning

---
+++ data_utilities.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import numpy as np
+from matplotlib import pyplot as pt
+import statistics as stats
+
+
+
+#slice_slices the data by row,this function returns rows x to y
+def slice(data,x,y):
+    return data.iloc[int(x)-1:int(y)-1,:]
+
+
+
+def data_iterator(epoch = 10):
+    """
+    method to interate through data set
+    :param epoch: number of rows per episode
+    :return:
+    """
+    num_rows = len(data.index)
+    num_iters = int(num_rows/epoch)
+    for i in range(num_iters):
+        return slice(data = data,x = epoch*i, y = epoch*i + epoch-1)
+
+
+####return data by date
+#data.loc[data.Date=="3/5/1970",:]
+
+##slices by date
+def dated(data,x,y):
+    i = data[data['Date']==x].index[0]
+    l = data[data['Date']==y].index[0]
+    return slice(data,i+1,l+2)
+#dated(data,"3/5/1970","3/10/1970")
+
+datas = slice(data,1,300)
+datas["Daily_Return_Pct"].plot()
+pt.show()
---
+++ distribution_utils.py
@@ -0,0 +1,24 @@
+import numpy as np
+import data_utilities as dt
+import statistics as stats
+import pandas as pd
+
+class Dist_Utils(object):
+
+     DATA = pd.read_csv("S&P500(1970-2020).csv")
+     RETURNS = DATA["Daily_Return_Pct"]
+
+
+
+
+     def confidence_interval(self):
+      pass
+
+     def fitter(self,data):
+         """
+         Method to fit data to distribution
+         :param data: data set to fit
+         :return:
+         """
+
+
